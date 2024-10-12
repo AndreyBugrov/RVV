a5edf3794bafe1026b83bf6d275ef1a01e355048
AndreyBugrov <bugrovandriy@yandex.ru>
2024-10-12 23:56:09 +0300

    Problem solved: kill experiment runner

--- experiments/experiment_task.hpp
+++ experiments/experiment_task.hpp
@@ -6,54 +6,90 @@
 #include "../algorithms/gram_schmidt.hpp"  // GramSchmidtInput
 #include "../algorithms/qr_decomposition.hpp"  // QRInput
 
-template<typename FunctionInput, typename FunctionOutput>
-class ExperimentTask: public BaseTask<FunctionInput, FunctionOutput, BaseTaskOutput>{
+// template<typename FunctionInput, typename FunctionOutput>
+// class ExperimentTask: public BaseTask<FunctionInput, FunctionOutput, BaseTaskOutput>{
+// protected:
+//     std::function<FunctionOutput(FunctionInput)> open_blas_task_;
+//     std::function<FunctionOutput(FunctionInput)> boost_task_;
+//     std::function<FunctionOutput(FunctionInput)> eigen_task_;
+
+// public:
+//     ExperimentTask(std::string name, std::function<FunctionOutput(FunctionInput)> task=dumb_task,
+//     std::function<FunctionOutput(FunctionInput)> open_blas_task=dumb_task,
+//     std::function<FunctionOutput(FunctionInput)> boost_task=dumb_task,
+//     std::function<FunctionOutput(FunctionInput)> eigen_task=dumb_task): 
+//     BaseTask<ExperimentFunctionInput, ExperimentFunctionOutput, BaseTaskOutput>(name, task),
+//     open_blas_task_(open_blas_task), boost_task_(boost_task), eigen_task_(eigen_task){}
+// };
+
+class NormalExperimentTask{
 protected:
-    std::function<FunctionOutput(FunctionInput)> open_blas_task_;
-    std::function<FunctionOutput(FunctionInput)> boost_task_;
-    std::function<FunctionOutput(FunctionInput)> eigen_task_;
-
+    std::string name_;
 public:
-    ExperimentTask(std::string name, std::function<FunctionOutput(FunctionInput)> task=dumb_task,
-    std::function<FunctionOutput(FunctionInput)> open_blas_task=dumb_task,
-    std::function<FunctionOutput(FunctionInput)> boost_task=dumb_task,
-    std::function<FunctionOutput(FunctionInput)> eigen_task=dumb_task): 
-    BaseTask<ExperimentFunctionInput, ExperimentFunctionOutput, BaseTaskOutput>(name, task),
-    open_blas_task_(open_blas_task), boost_task_(boost_task), eigen_task_(eigen_task){}
+    NormalExperimentTask(std::string name): 
+    name_(name){}
+    template<typename Foo, typename... Args>
+    BaseTaskOutput run(Foo task, Args... args) const{
+        bool ended = false;
+        std::string error_type = "NoError";
+        std::string error_message = "Congratulations!";
+        double seconds = 0.0;
+        try{
+            const auto start_test{std::chrono::steady_clock::now()};
+            task(args...);
+            const auto end_test{std::chrono::steady_clock::now()};
+            ended = true;
+            std::chrono::duration<double> test_seconds = end_test - start_test;
+            seconds = test_seconds.count();
+        }
+        catch(Exception my_error){
+            error_type = my_error.what();
+            error_message = my_error.message();
+        }
+        catch(std::exception ex){
+            error_type = ex.what();
+            error_message = "Unknown";
+        }
+        catch(...){
+            error_type = "Unknown";
+            error_message = "Unknown";
+        }
+        return BaseTaskOutput(ended, error_type, error_message, seconds);
+    }
 };
 
-class VectorTask: ExperimentTask<VectorProdInput, double>{
-public:
-    VectorTask(std::string name, std::function<double(VectorProdInput)> task=dumb_task<VectorProdInput, double>,
-    std::function<double(VectorProdInput)> open_blas_task=dumb_task<VectorProdInput, double>,
-    std::function<double(VectorProdInput)> boost_task=dumb_task<VectorProdInput, double>,
-    std::function<double(VectorProdInput)> eigen_task=dumb_task<VectorProdInput, double>): 
-    ExperimentTask<VectorProdInput, double>(name, task, open_blas_task, boost_task, eigen_task){}
-};
+// class VectorTask: ExperimentTask<VectorProdInput, double>{
+// public:
+//     VectorTask(std::string name, std::function<double(VectorProdInput)> task=dumb_task<VectorProdInput, double>,
+//     std::function<double(VectorProdInput)> open_blas_task=dumb_task<VectorProdInput, double>,
+//     std::function<double(VectorProdInput)> boost_task=dumb_task<VectorProdInput, double>,
+//     std::function<double(VectorProdInput)> eigen_task=dumb_task<VectorProdInput, double>): 
+//     ExperimentTask<VectorProdInput, double>(name, task, open_blas_task, boost_task, eigen_task){}
+// };
 
-class MatrixTask: ExperimentTask<MatrixProdInput, void>{
-public:
-    MatrixTask(std::string name, std::function<void(MatrixProdInput)> task=dumb_task<MatrixProdInput, void>,
-    std::function<void(MatrixProdInput)> open_blas_task=dumb_task<MatrixProdInput, void>,
-    std::function<void(MatrixProdInput)> boost_task=dumb_task<MatrixProdInput, void>,
-    std::function<void(MatrixProdInput)> eigen_task=dumb_task<MatrixProdInput, void>):
-    ExperimentTask<MatrixProdInput, void>(name, task, open_blas_task, boost_task, eigen_task){}
-};
+// class MatrixTask: ExperimentTask<MatrixProdInput, void>{
+// public:
+//     MatrixTask(std::string name, std::function<void(MatrixProdInput)> task=dumb_task<MatrixProdInput, void>,
+//     std::function<void(MatrixProdInput)> open_blas_task=dumb_task<MatrixProdInput, void>,
+//     std::function<void(MatrixProdInput)> boost_task=dumb_task<MatrixProdInput, void>,
+//     std::function<void(MatrixProdInput)> eigen_task=dumb_task<MatrixProdInput, void>):
+//     ExperimentTask<MatrixProdInput, void>(name, task, open_blas_task, boost_task, eigen_task){}
+// };
 
-class GramSchmidtTask: ExperimentTask<GramSchmidtInput, void>{
-public:
-    GramSchmidtTask(std::string name, std::function<void(GramSchmidtInput)> task=dumb_task<GramSchmidtInput, void>,
-    std::function<void(GramSchmidtInput)> open_blas_task=dumb_task<GramSchmidtInput, void>,
-    std::function<void(GramSchmidtInput)> boost_task=dumb_task<GramSchmidtInput, void>,
-    std::function<void(GramSchmidtInput)> eigen_task=dumb_task<GramSchmidtInput, void>): 
-    ExperimentTask<GramSchmidtInput, void>(name, task, open_blas_task, boost_task, eigen_task){}
-};
+// class GramSchmidtTask: ExperimentTask<GramSchmidtInput, void>{
+// public:
+//     GramSchmidtTask(std::string name, std::function<void(GramSchmidtInput)> task=dumb_task<GramSchmidtInput, void>,
+//     std::function<void(GramSchmidtInput)> open_blas_task=dumb_task<GramSchmidtInput, void>,
+//     std::function<void(GramSchmidtInput)> boost_task=dumb_task<GramSchmidtInput, void>,
+//     std::function<void(GramSchmidtInput)> eigen_task=dumb_task<GramSchmidtInput, void>): 
+//     ExperimentTask<GramSchmidtInput, void>(name, task, open_blas_task, boost_task, eigen_task){}
+// };
 
-class QRTask: ExperimentTask<QRInput, void>{
-public:
-    QRTask(std::string name, std::function<void(QRInput)> task=dumb_task<QRInput, void>,
-    std::function<void(QRInput)> open_blas_task=dumb_task<QRInput, void>,
-    std::function<void(QRInput)> boost_task=dumb_task<QRInput, void>,
-    std::function<void(QRInput)> eigen_task=dumb_task<QRInput, void>):
-    ExperimentTask<QRInput, void>(name, task, open_blas_task, boost_task, eigen_task){}
-};
+// class QRTask: ExperimentTask<QRInput, void>{
+// public:
+//     QRTask(std::string name, std::function<void(QRInput)> task=dumb_task<QRInput, void>,
+//     std::function<void(QRInput)> open_blas_task=dumb_task<QRInput, void>,
+//     std::function<void(QRInput)> boost_task=dumb_task<QRInput, void>,
+//     std::function<void(QRInput)> eigen_task=dumb_task<QRInput, void>):
+//     ExperimentTask<QRInput, void>(name, task, open_blas_task, boost_task, eigen_task){}
+// };
--- experiments/main_experiment.cpp
+++ experiments/main_experiment.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
 
-#include "experiment_runner.hpp"
+#include "experiment_task.hpp"
+
+double abobus(double a, double b, bool yes, int number){
+    return yes? pow(a, b)*sin(a)/cos(b)+number: (1/pow(a, 1/b))*pow(b, tan(a))/number;
+}
 
 int main(){
-
-    ExperimentRunner runner();
+    // recognize function type
+    // get arguments
+    //map<string name, enum class function_index>
+    //switch by function_index
+    //run task
+    NormalExperimentTask task(std::string("good task"));
+    BaseTaskOutput output = task.run(abobus, 7.5, -3.2, false, 5);
+    std::cout<<"ended: "<<output.ended()<<"\n";
+    std::cout<<"seconds: "<<output.time()<<"\n";
+    std::cout<<"what: "<<output.what()<<"\n";
+    std::cout<<"error message: "<<output.error_message()<<"\n";
     return 0;
 }
