4e2d7a14c888423a6cec198531b05765b7238355
AndreyBugrov <bugrovandriy@yandex.ru>
2024-11-02 14:36:31 +0300

    added logger but did not tested

---
+++ common/single_logger.cpp
@@ -0,0 +1,222 @@
+#include "single_logger.hpp"
+
+std::unordered_map<FormatItem, string> SingleLogger::format_converter = {
+    std::pair<FormatItem, string>(FormatItem::kMessage, "%m"),
+    std::pair<FormatItem, string>(FormatItem::kFunction, "%f"),
+    std::pair<FormatItem, string>(FormatItem::kFile, "%u"), // unit
+    std::pair<FormatItem, string>(FormatItem::kTime, "%t"),
+    std::pair<FormatItem, string>(FormatItem::kLine, "%r"),  // row
+    std::pair<FormatItem, string>(FormatItem::kColumn, "%c"),
+    std::pair<FormatItem, string>(FormatItem::kLevel, "%l")
+};
+
+SingleLogger* SingleLogger::self_ = nullptr;
+
+void SingleLogger::log(InnerLoggerLevel level, const string& message, const source_location& location) const noexcept{
+    if(log_level_ <= level){
+        print_log(message, location);
+    }
+    if(terminate_level_ <= level){
+        std::abort();
+    }
+}
+
+void SingleLogger::debug(const string& message, const source_location& location) const noexcept{
+    log(InnerLoggerLevel::kDebug, message, location);
+}
+void SingleLogger::info(const string& message, const source_location& location) const noexcept{
+    log(InnerLoggerLevel::kInfo, message, location);
+}
+void SingleLogger::warning(const string& message, const source_location& location) const noexcept{
+    log(InnerLoggerLevel::kWarning, message, location);
+}
+void SingleLogger::error(const string& message, const source_location& location) const noexcept{
+    log(InnerLoggerLevel::kError, message, location);
+}
+void SingleLogger::critical(const string& message, const source_location& location) const noexcept{
+    log(InnerLoggerLevel::kCritical, message, location);
+}
+
+void SingleLogger::set_log_level(LoggerLevel level){
+    switch (level)
+    {
+    case LoggerLevel::kDebug:
+        log_level_ = InnerLoggerLevel::kDebug;
+        break;
+    case LoggerLevel::kInfo:
+        log_level_ = InnerLoggerLevel::kInfo;
+        break;
+    case LoggerLevel::kWarning:
+        log_level_ = InnerLoggerLevel::kWarning;
+        break;
+    case LoggerLevel::kError:
+        log_level_ = InnerLoggerLevel::kError;
+        break;
+    case LoggerLevel::kCritical:
+        log_level_ = InnerLoggerLevel::kCritical;
+        break;
+    default:
+        Exception exception(ErrorType::kValueError, generate_string("Unknown number of LoggerLevel variable value: ", static_cast<int>(level)));
+        throw exception;
+        break;
+    }
+}
+void SingleLogger::set_terminate_level(LoggerLevel level){
+    switch (level)
+    {
+    case LoggerLevel::kDebug:
+        terminate_level_ = InnerLoggerLevel::kDebug;
+        break;
+    case LoggerLevel::kInfo:
+        terminate_level_ = InnerLoggerLevel::kInfo;
+        break;
+    case LoggerLevel::kWarning:
+        terminate_level_ = InnerLoggerLevel::kWarning;
+        break;
+    case LoggerLevel::kError:
+        terminate_level_ = InnerLoggerLevel::kError;
+        break;
+    case LoggerLevel::kCritical:
+        terminate_level_ = InnerLoggerLevel::kCritical;
+        break;
+    default:
+        Exception exception(ErrorType::kValueError, generate_string("Unknown number of LoggerLevel variable value: ", static_cast<int>(level)));
+        throw exception;
+        break;
+    }
+}
+
+void SingleLogger::unset_terminate_level(){
+    terminate_level_ = InnerLoggerLevel::kNoLevel;
+}
+
+string SingleLogger::get_terminate_level() const noexcept{
+    switch (terminate_level_)
+    {
+    case InnerLoggerLevel::kDebug:
+        return "DEBUG";
+    case InnerLoggerLevel::kInfo:
+        return "INFO";
+        break;
+    case InnerLoggerLevel::kWarning:
+        return "WARNING";
+        break;
+    case InnerLoggerLevel::kError:
+        return "ERROR";
+        break;
+    case InnerLoggerLevel::kCritical:
+        return "CRITICAL";
+    case InnerLoggerLevel::kNoLevel:
+        return "NO LEVEL";
+    default:
+        Exception exception(ErrorType::kValueError, generate_string("Unsupported number of InnerLoggerLevel variable value: ", log_level_));
+        break;
+    }
+}
+
+void SingleLogger::set_output_stream(std::ostream& stream){
+    stream_ = &stream;
+}
+
+SingleLogger* SingleLogger::get_instance(){
+    if(!self_){
+        self_ = new SingleLogger();
+    }
+    return self_;
+}
+
+void SingleLogger::destroy_instance(){
+    if(self_){
+        delete self_;
+    }
+}
+
+void SingleLogger::set_log_format(const string& format){
+
+}
+
+string SingleLogger::get_log_format() const noexcept{
+    switch (log_level_)
+    {
+    case InnerLoggerLevel::kDebug:
+        return "DEBUG";
+    case InnerLoggerLevel::kInfo:
+        return "INFO";
+        break;
+    case InnerLoggerLevel::kWarning:
+        return "WARNING";
+        break;
+    case InnerLoggerLevel::kError:
+        return "ERROR";
+        break;
+    case InnerLoggerLevel::kCritical:
+        return "CRITICAL";
+    case InnerLoggerLevel::kNoLevel:
+        return "NO LEVEL";
+    default:
+        Exception exception(ErrorType::kValueError, generate_string("Unsupported number of InnerLoggerLevel variable value: ", log_level_));
+        break;
+    }
+}
+
+string SingleLogger::get_log_level() const noexcept{
+    string output = "";
+    for(string lexem : log_format_){
+        output.append(lexem);
+    }
+    return output;
+}
+
+void SingleLogger::print_log(const string& message, const source_location& location) const noexcept{
+    for(std::string lexem: log_format_){
+        if(lexem == "%m"){
+            stream_->operator<<(message.data());
+        }
+        if(lexem == "%f"){
+            stream_<<location.function_name();
+            continue;
+        }
+        if(lexem == "%u"){
+            stream_->operator<<(location.file_name());
+            continue;
+        }
+        if(lexem == "%t"){
+            auto current_date = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+            stream_->operator<<(::ctime(&current_date));
+            continue;
+        }
+        if(lexem == "%r"){
+            stream_->operator<<(location.line());
+            continue;
+        }
+        if(lexem == "%c"){
+            stream_->operator<<(location.column());
+            continue;
+        }
+        if(lexem == "%l"){
+            switch (log_level_)
+            {
+            case InnerLoggerLevel::kDebug:
+                stream_->operator<<("DEBUG");
+                break;
+            case InnerLoggerLevel::kInfo:
+                stream_->operator<<("INFO");
+                break;
+            case InnerLoggerLevel::kWarning:
+                stream_->operator<<("WARNING");
+                break;
+            case InnerLoggerLevel::kError:
+                stream_->operator<<("ERROR");
+                break;
+            case InnerLoggerLevel::kCritical:
+                stream_->operator<<("CRITICAL");
+            default:
+                Exception exception(ErrorType::kValueError, generate_string("Unsupported number of InnerLoggerLevel variable value: ", log_level_));
+                break;
+            }
+            continue;
+        }
+        stream_->operator<<(lexem.data());
+    }
+    stream_->operator<<("\n");
+}
---
+++ common/single_logger.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <cstdlib>  // std::abort
+
+#include <string>  // log strings
+#include <vector>  // log format
+#include <iostream>  // log output
+#include <source_location>  // location of code
+#include <unordered_map>  // format converter
+#include <ctime>  // get current date and time
+#include <chrono>  // get current date and time
+
+#include "exception.hpp"  // exceptions
+#include "generators.hpp"  // string generator
+ 
+using std::string;
+using std::source_location;
+
+enum class LoggerLevel{
+    kDebug,
+    kInfo,
+    kWarning,
+    kError,
+    kCritical,
+};
+
+enum class FormatItem{
+    kMessage,
+    kLevel,
+    kFile,
+    kFunction,
+    kTime,
+    kLine,
+    kColumn,
+};
+
+class SingleLogger{
+public:
+    static SingleLogger* get_instance();
+    static void destroy_instance();
+    static std::unordered_map<FormatItem, string> format_converter;
+
+    void debug(const string& message, const source_location& location) const noexcept;
+    void info(const string& message, const source_location& location) const noexcept;
+    void warning(const string& message, const source_location& location) const noexcept;
+    void error(const string& message, const source_location& location) const noexcept;
+    void critical(const string& message, const source_location& location) const noexcept;
+
+    void set_log_level(LoggerLevel level);
+    string get_log_level() const noexcept;
+    void set_log_format(const string& format);
+    string get_log_format() const noexcept;
+    void set_terminate_level(LoggerLevel level);
+    void unset_terminate_level();
+    string get_terminate_level() const noexcept;
+    void set_output_stream(std::ostream& stream);
+
+
+private:
+    enum InnerLoggerLevel{
+        kDebug = 0,
+        kInfo,
+        kWarning,
+        kError,
+        kCritical,
+        kNoLevel,
+    };
+    static SingleLogger* self_;
+    std::ostream* stream_;
+    size_t log_level_;
+    size_t terminate_level_;
+    std::vector<string> log_format_;
+
+    SingleLogger(): stream_(&std::cout), log_level_(kNoLevel), terminate_level_(kNoLevel), log_format_{"%l", "%m"}{}
+    virtual ~SingleLogger(){}
+    SingleLogger(const SingleLogger& logger) = delete;
+    SingleLogger& operator=(const SingleLogger& logger) = delete;
+
+    void print_log(const string& message, const source_location& location) const noexcept;
+    void log(InnerLoggerLevel level, const string& message, const source_location& location) const noexcept;
+};
