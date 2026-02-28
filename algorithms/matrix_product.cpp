cf80015d67a3432818c131c00d2efba73e8715bc
AndreyBugrov <bugrovandriy@yandex.ru>
2026-02-28 08:13:17 +0300

    banana 3

--- algorithms/matrix_product.cpp
+++ algorithms/matrix_product.cpp
@@ -63,7 +63,6 @@ void matrix_product_row_block(const vector<num_type>& a, const vector<num_type>&
 
 void matrix_product_row_block_scalar(const vector<num_type>& a, const vector<num_type>& b, vector<num_type>& c, size_t a_row_count, size_t a_column_count, size_t b_column_count){
     check_length(a.size(), b.size(), c.size(), a_row_count, a_column_count, b_column_count);
-    // #pragma omp parallel for
     for(size_t ik = 0; ik < a_row_count; ik += kBlockSize){
         for(size_t jk = 0; jk < a_column_count; jk += kBlockSize){
             for(size_t kk = 0; kk < b_column_count; kk += kBlockSize){
@@ -77,18 +76,37 @@ void matrix_product_row_block_scalar(const vector<num_type>& a, const vector<num
     }
 }
 
+// void matrix_product_row_block_scalar_par(const vector<num_type>& a, const vector<num_type>& b, vector<num_type>& c, size_t a_row_count, size_t a_column_count, size_t b_column_count){
+//     check_length(a.size(), b.size(), c.size(), a_row_count, a_column_count, b_column_count);
+//     size_t ik, jk, kk, i, j;
+//     #pragma omp parallel for shared(a, b, c, a_row_count, a_column_count, b_column_count, kBlockSize) private(ik, jk, kk, i, j)
+//     for(ik = 0; ik < a_row_count; ik += kBlockSize){
+//         for(jk = 0; jk < a_column_count; jk += kBlockSize){
+//             for(kk = 0; kk < b_column_count; kk += kBlockSize){
+//                 for(i = 0; i < kBlockSize; ++i){
+//                     for(j = 0; j < kBlockSize; ++j){
+//                         matrix_multiply_vector_by_number_optimal(b.data() + (jk + j) * b_column_count + (kk), c.data() + (ik + i) * b_column_count + (kk), a[(ik + i) * a_column_count + (jk + j)], kBlockSize);
+//                     }
+//                 }  
+//             }
+//         }
+//     }
+// }
+
 void matrix_product_row_block_scalar_par(const vector<num_type>& a, const vector<num_type>& b, vector<num_type>& c, size_t a_row_count, size_t a_column_count, size_t b_column_count){
     check_length(a.size(), b.size(), c.size(), a_row_count, a_column_count, b_column_count);
-    size_t ik, jk, kk, i, j;
-    #pragma omp parallel for shared(a, b, c, a_row_count, a_column_count, b_column_count, kBlockSize) private(ik, jk, kk, i, j)
+    size_t ik, jk, kk, i, j, k;
+    #pragma omp parallel for shared(a, b, c, a_row_count, a_column_count, b_column_count, kBlockSize) private(ik, jk, kk, i, j, k)
     for(ik = 0; ik < a_row_count; ik += kBlockSize){
         for(jk = 0; jk < a_column_count; jk += kBlockSize){
             for(kk = 0; kk < b_column_count; kk += kBlockSize){
                 for(i = 0; i < kBlockSize; ++i){
                     for(j = 0; j < kBlockSize; ++j){
-                        matrix_multiply_vector_by_number_optimal(b.data() + (jk + j) * b_column_count + (kk), c.data() + (ik + i) * b_column_count + (kk), a[(ik + i) * a_column_count + (jk + j)], kBlockSize);
+                        for(k = 0; k < kBlockSize; ++k){
+                            c[(ik + i) * b_column_count + (kk + k)] += a[(ik + i) * a_column_count + (jk + j)] * b[(jk + j) * b_column_count + (kk + k)];
+                        }
                     }
-                }  
+                }
             }
         }
     }
