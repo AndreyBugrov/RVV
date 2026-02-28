33763886548daa807a7afb45abd6403b43b5c57f
AndreyBugrov <bugrovandriy@yandex.ru>
2026-02-28 09:30:45 +0300

    banana 6

--- algorithms/matrix_product.cpp
+++ algorithms/matrix_product.cpp
@@ -76,6 +76,26 @@ void matrix_product_row_block_scalar(const vector<num_type>& a, const vector<num
     }
 }
 
+void matrix_product_row_block_scalar_par(const vector<num_type>& a, const vector<num_type>& b, vector<num_type>& c, size_t a_row_count, size_t a_column_count, size_t b_column_count){
+    check_length(a.size(), b.size(), c.size(), a_row_count, a_column_count, b_column_count);
+    size_t ik, jk, kk, i, j, k;
+    #pragma omp parallel for shared(a, b, c, a_row_count, a_column_count, b_column_count, kBlockSize) private(ik, jk, kk, i, j, k)
+    for(ik = 0; ik < a_row_count; ik += kBlockSize){
+        for(jk = 0; jk < a_column_count; jk += kBlockSize){
+            for(kk = 0; kk < b_column_count; kk += kBlockSize){
+                for(i = 0; i < kBlockSize; ++i){
+                    for(j = 0; j < kBlockSize; ++j){
+                        for(k = 0; k < kBlockSize; ++k){
+                            c[(ik + i) * b_column_count + (kk + k)] += a[(ik + i) * a_column_count + (jk + j)] * b[(jk + j) * b_column_count + (kk + k)];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// bad on board
 // void matrix_product_row_block_scalar_par(const vector<num_type>& a, const vector<num_type>& b, vector<num_type>& c, size_t a_row_count, size_t a_column_count, size_t b_column_count){
 //     check_length(a.size(), b.size(), c.size(), a_row_count, a_column_count, b_column_count);
 //     size_t ik, jk, kk, i, j;
@@ -85,55 +105,19 @@ void matrix_product_row_block_scalar(const vector<num_type>& a, const vector<num
 //             for(kk = 0; kk < b_column_count; kk += kBlockSize){
 //                 for(i = 0; i < kBlockSize; ++i){
 //                     for(j = 0; j < kBlockSize; ++j){
-//                         matrix_multiply_vector_by_number_optimal(b.data() + (jk + j) * b_column_count + (kk), c.data() + (ik + i) * b_column_count + (kk), a[(ik + i) * a_column_count + (jk + j)], kBlockSize);
-//                     }
-//                 }  
-//             }
-//         }
-//     }
-// }
-
-// void matrix_product_row_block_scalar_par(const vector<num_type>& a, const vector<num_type>& b, vector<num_type>& c, size_t a_row_count, size_t a_column_count, size_t b_column_count){
-//     check_length(a.size(), b.size(), c.size(), a_row_count, a_column_count, b_column_count);
-//     size_t ik, jk, kk, i, j, k;
-//     #pragma omp parallel for shared(a, b, c, a_row_count, a_column_count, b_column_count, kBlockSize) private(ik, jk, kk, i, j, k)
-//     for(ik = 0; ik < a_row_count; ik += kBlockSize){
-//         for(jk = 0; jk < a_column_count; jk += kBlockSize){
-//             for(kk = 0; kk < b_column_count; kk += kBlockSize){
-//                 for(i = 0; i < kBlockSize; ++i){
-//                     for(j = 0; j < kBlockSize; ++j){
-//                         for(k = 0; k < kBlockSize; ++k){
-//                             c[(ik + i) * b_column_count + (kk + k)] += a[(ik + i) * a_column_count + (jk + j)] * b[(jk + j) * b_column_count + (kk + k)];
+//                         for(size_t i=0;i<kBlockSize;i+=kUnrollCoefficient){
+//                             c[(ik + i) * b_column_count + (kk) + i] += b[(jk + j) * b_column_count + (kk) + i] * a[(ik + i) * a_column_count + (jk + j)];
+//                             c[(ik + i) * b_column_count + (kk) + i + 1] += b[(jk + j) * b_column_count + (kk) + i + 1] * a[(ik + i) * a_column_count + (jk + j)];
+//                             c[(ik + i) * b_column_count + (kk) + i + 2] += b[(jk + j) * b_column_count + (kk) + i + 2] * a[(ik + i) * a_column_count + (jk + j)];
+//                             c[(ik + i) * b_column_count + (kk) + i + 3] += b[(jk + j) * b_column_count + (kk) + i + 3] * a[(ik + i) * a_column_count + (jk + j)];
 //                         }
+//                         // matrix_multiply_vector_by_number_optimal(b.data() + (jk + j) * b_column_count + (kk), c.data() + (ik + i) * b_column_count + (kk), a[(ik + i) * a_column_count + (jk + j)], kBlockSize);
+//                         // for(k = 0; k < kBlockSize; ++k){
+//                         //     c[(ik + i) * b_column_count + (kk + k)] += a[(ik + i) * a_column_count + (jk + j)] * b[(jk + j) * b_column_count + (kk + k)];
+//                         // }
 //                     }
 //                 }
 //             }
 //         }
 //     }
 // }
-
-void matrix_product_row_block_scalar_par(const vector<num_type>& a, const vector<num_type>& b, vector<num_type>& c, size_t a_row_count, size_t a_column_count, size_t b_column_count){
-    check_length(a.size(), b.size(), c.size(), a_row_count, a_column_count, b_column_count);
-    size_t ik, jk, kk, i, j;
-    #pragma omp parallel for shared(a, b, c, a_row_count, a_column_count, b_column_count, kBlockSize) private(ik, jk, kk, i, j)
-    for(ik = 0; ik < a_row_count; ik += kBlockSize){
-        for(jk = 0; jk < a_column_count; jk += kBlockSize){
-            for(kk = 0; kk < b_column_count; kk += kBlockSize){
-                for(i = 0; i < kBlockSize; ++i){
-                    for(j = 0; j < kBlockSize; ++j){
-                        for(size_t i=0;i<kBlockSize;i+=kUnrollCoefficient){
-                            c[(ik + i) * b_column_count + (kk) + i] += b[(jk + j) * b_column_count + (kk) + i] * a[(ik + i) * a_column_count + (jk + j)];
-                            c[(ik + i) * b_column_count + (kk) + i + 1] += b[(jk + j) * b_column_count + (kk) + i + 1] * a[(ik + i) * a_column_count + (jk + j)];
-                            c[(ik + i) * b_column_count + (kk) + i + 2] += b[(jk + j) * b_column_count + (kk) + i + 2] * a[(ik + i) * a_column_count + (jk + j)];
-                            c[(ik + i) * b_column_count + (kk) + i + 3] += b[(jk + j) * b_column_count + (kk) + i + 3] * a[(ik + i) * a_column_count + (jk + j)];
-                        }
-                        // matrix_multiply_vector_by_number_optimal(b.data() + (jk + j) * b_column_count + (kk), c.data() + (ik + i) * b_column_count + (kk), a[(ik + i) * a_column_count + (jk + j)], kBlockSize);
-                        // for(k = 0; k < kBlockSize; ++k){
-                        //     c[(ik + i) * b_column_count + (kk + k)] += a[(ik + i) * a_column_count + (jk + j)] * b[(jk + j) * b_column_count + (kk + k)];
-                        // }
-                    }
-                }
-            }
-        }
-    }
-}
